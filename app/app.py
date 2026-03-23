5ee2e307724eacc79a02e9f3cff727c57d68275b
psa21git <psa.dev.21@gmail.com>
2026-03-23 18:32:47 +0530

    models

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from app.routes import predict_bp
 import os
+import pickle
 
 def create_app():
     app = Flask(__name__,
@@ -8,5 +9,16 @@ def create_app():
                 static_folder=os.path.join(os.getcwd(), "static"),
                 static_url_path="/static"
         )
+
+    # ✅ LOAD MODEL HERE (inside create_app)
+    model = pickle.load(open("models/credit_model.pkl", "rb"))
+    scaler = pickle.load(open("models/scaler.pkl", "rb"))
+    features = pickle.load(open("models/features.pkl", "rb"))
+
+    # store in app (so routes can access)
+    app.model = model
+    app.scaler = scaler
+    app.features = features
+
     app.register_blueprint(predict_bp)
     return app
